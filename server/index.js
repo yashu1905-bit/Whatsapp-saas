712e91414e090d5fe9db15b7519b85093e54d8b6
yashu1905-bit <yashuu1910@gmail.com>
2026-03-23 19:56:18 +0530

    update index file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import app from "./src/app.js"
 import config from "./src/config/index.js"
+import connectDB from "./src/database/mongo.db.js"
+
 
 
 // http://localhost:4001
-app.listen(config.PORT, config.HOST, () => {
+app.listen(config.PORT, () => {
     console.log(`Server up and running on http://${config.HOST}:${config.PORT}`)
+    connectDB();
 })
